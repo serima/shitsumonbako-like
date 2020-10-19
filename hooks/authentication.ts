a4e31f85fdbc779b8b1759921287727d296ec8e3
Ryo Shibayama <j02521@gmail.com>
2020-10-20 02:35:46 +0900

    Setting up users

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -40,6 +40,7 @@ export function useAuthentication() {
                 const loginUser: User = {
                     uid: firebaseUser.uid,
                     isAnonymous: firebaseUser.isAnonymous,
+                    name: '',
                 }
                 setUser(loginUser)
                 createUserIfNotFound(loginUser)
